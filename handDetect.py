0be9d88cc1235b8208cc6cdf6f1cdeb73f79655d
Courbot Lecerf Antoine <courbot.antoine@gmail.com>
2022-05-24 10:17:53 +0200

    use of google's  mediapipe library

---
+++ handDetect.py
@@ -0,0 +1,51 @@
+
+import mediapipe as mp
+import cv2 as cv
+import glob
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+IMAGE_FILES = []
+for image in glob.glob('BDD/*.bmp'):
+    IMAGE_FILES.append(image)
+
+
+with mp_hands.Hands(
+    static_image_mode=True,
+    max_num_hands=2,
+    min_detection_confidence=0.5) as hands:
+  for idx, file in enumerate(IMAGE_FILES):
+    # Read an image, flip it around y-axis for correct handedness output (see
+    # above).
+    image = cv.flip(cv.imread(file), 1)
+    # Convert the BGR image to RGB before processing.
+    results = hands.process(cv.cvtColor(image, cv.COLOR_BGR2RGB))
+
+    # Print handedness and draw hand landmarks on the image.
+    print('Handedness:', results.multi_handedness)
+    if not results.multi_hand_landmarks:
+      continue
+    image_height, image_width, _ = image.shape
+    annotated_image = image.copy()
+    for hand_landmarks in results.multi_hand_landmarks:
+     """  print('hand_landmarks:', hand_landmarks)
+      print(
+          f'Index finger tip coordinates: (',
+          f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, '
+          f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})'
+      ) """
+    mp_drawing.draw_landmarks(
+          annotated_image,
+          hand_landmarks,
+          mp_hands.HAND_CONNECTIONS,
+          mp_drawing_styles.get_default_hand_landmarks_style(),
+          mp_drawing_styles.get_default_hand_connections_style()) 
+    cv.imwrite(
+        "ROI/ROI_" + str(idx) + '.png', cv.flip(annotated_image, 1))
+    # Draw hand world landmarks.
+    print(str(idx))
+    if not results.multi_hand_world_landmarks:
+      continue
+    for hand_world_landmarks in results.multi_hand_world_landmarks:
+      """ mp_drawing.plot_landmarks(
+        hand_world_landmarks, mp_hands.HAND_CONNECTIONS, azimuth=5) """
---
+++ testHOG.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  8 14:26:36 2022
+
+@author: Utilisateur
+"""
+
+import cv2
+img = cv2.imread("Masques/masque_boeuf1.bmp",0)
+gx = cv2.Sobel(img, cv2.CV_32F, 1, 0)
+gy = cv2.Sobel(img, cv2.CV_32F, 0, 1)
+cv2.imshow("gx", gx)
+mag, ang = cv2.cartToPolar(gx, gy)
+
+winSize = (64,64)
+blockSize = (16,16)
+blockStride = (8,8)
+cellSize = (8,8)
+nbins = 9
+derivAperture = 1
+winSigma = 4.
+histogramNormType = 0
+L2HysThreshold = 2.0000000000000001e-01
+gammaCorrection = 0
+nlevels = 64
+hog = cv2.HOGDescriptor(winSize,blockSize,blockStride,cellSize,nbins,derivAperture,winSigma,
+                        histogramNormType,L2HysThreshold,gammaCorrection,nlevels)
+#compute(img[, winStride[, padding[, locations]]]) -> descriptors
+winStride = (8,8)
+padding = (8,8)
+locations = ((10,20),)
+hist = hog.compute(img,winStride,padding,locations)
